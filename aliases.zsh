7179c94bb04307b38a713777f752a272ebd7eb18
Sushil Shinde <shinde.sushil@gmail.com>
2018-07-03 12:30:48 +0530

    Add files via upload

---
+++ aliases.zsh
@@ -0,0 +1,43 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+alias dir='ls'
+
+alias pt="~/projects/tc"
+alias pl="~/projects/local"
+alias c="clear"
+
+alias ..="cd .."
+alias cd..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+alias g="git"
+alias gi='git init'
+alias gp='git push'
+alias gpl='git pull'
+alias ga='git add .'
+alias gc='git commit -m'
+alias ooanward='git commit -am'
+alias ge='git checkout .'
+
+alias v="vim"
+
+alias a="atom ."
+alias reload="source ~/.zshrc"
+alias ze="vim ~/.zshrc"
+alias al="cat $ZSH/custom/aliases.zsh"
+alias ae="vim $ZSH/custom/aliases.zsh"
